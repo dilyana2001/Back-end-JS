bff2fb481de5b1e7f9f6e2b1437c335279ba339d
dilyana2001 <dilyana2001@gmail.com>
2021-09-25 11:04:29 +0300

    add details page

--- cubicle/controllers/productController.js
+++ cubicle/controllers/productController.js
@@ -1,7 +1,16 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => res.render('home', { title: 'Cubicle' }));
-router.get('/create', (req, res) => res.render('create', { title: 'Create Cube Page' }));
+router.get('/', (req, res) => {
+    res.render('home', { title: 'Cubicle' })
+});
+router.get('/create', (req, res) => {
+    res.render('create', { title: 'Create Cube Page' })
+});
+router.get('/details/:productId', (req, res) => {
+    console.log(req.params.productId);
+    res.render('details', { title: 'Cubicle' })
+})
+
 
 module.exports = router;
