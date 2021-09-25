b5a702a017c1c812812c08912aea2b86bedece1e
dilyana2001 <dilyana2001@gmail.com>
2021-09-25 11:21:09 +0300

    redirect to home page

--- cubicle/controllers/productController.js
+++ cubicle/controllers/productController.js
@@ -9,15 +9,17 @@ router.get('/create', (req, res) => {
     res.render('create', { title: 'Create Cube Page' })
 });
 
+router.post('/create', (req, res) => {
+    console.log(req.body);
+
+    res.redirect('/')
+});
+
 router.get('/details/:productId', (req, res) => {
     console.log(req.method, req.params.productId);
     res.render('details', { title: 'Cubicle' })
 });
 
-router.post('/create', (req, res) => {
-    console.log(req.body);
-    res.send('created');
-})
 
 
 
